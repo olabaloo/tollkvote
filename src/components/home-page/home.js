696517c7d0ce8c7f21e2856a73a673f08ce46ddf
Ola Imerslund <ola.imerslund@gyldendal.no>
2015-04-04 12:28:51 +0200

    Initial commit

---
+++ src/components/home-page/home.js
@@ -0,0 +1,63 @@
+define(["knockout", "jquery", "text!./home.html"], function(ko, $, homeTemplate) {
+  'use strict';
+  function HomeViewModel(route) {
+    this.INVALID = -1;
+    this.DUBIUOS = 0;
+    this.VALID = 1;
+    this.limit = 0;
+    this.numberOfLitres = ko.observable(0);
+    this.valid = ko.observable(this.DUBIUOS);
+    this.previousValid = this.DUBIUOS;
+    this.loadData();
+    this.formats = ko.observableArray();
+    this.units = ko.observableArray();
+    var self=this;
+//    this.numberOfLitres.subscribe(this.validate());
+}
+
+HomeViewModel.prototype.loadData = function() {
+  var that = this;
+  $.getJSON("/data/data.json", function(jsondata) {
+    that.formats(jsondata.formats);
+    that.units(jsondata.units);
+  });
+};
+
+HomeViewModel.prototype.add = function(binder) {
+  this.update(binder.size);
+  console.dir(this);
+};
+
+HomeViewModel.prototype.subtract = function(binder) {
+  this.update(-binder.size);
+};
+
+HomeViewModel.prototype.update = function(addition) {
+  var newNumberOfLitres = this.numberOfLitres() + addition;
+  this.numberOfLitres(newNumberOfLitres);
+    //this.validate(0);
+  };
+
+  HomeViewModel.prototype.validate = function() {
+    this.previousValid = this.valid();
+    var currentNumberOfLitres = this.numberOfLitres();
+    if(currentNumberOfLitres === this.limit) {
+      this.valid(this.DUBIUOS);
+    } else if(currentNumberOfLitres > this.limit) {
+      this.valid(this.VALID);
+    } else {
+      this.valid(this.INVALID);
+    }
+
+    if(this.valid() === this.DUBIUOS) {
+      console.log('Dubiuous.');
+    } else if(this.valid() === this.INVALID && this.previousValid !== this.INVALID) {
+      console.log('Invalid.');
+    } else if(this.valid() === this.VALID && this.previousValid !== this.VALID) {
+      console.log('Valid.');
+    }
+  };
+
+  return { viewModel: HomeViewModel, template: homeTemplate };
+
+});
